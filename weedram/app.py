e3f39c637f93b5d70b51e6d570d8447564bdccc2
Simon Cross <hodgestar+github@gmail.com>
2015-07-30 15:00:24 +0200

    Fix reference to os.environ (@rudigiesler @komanelson @hodgestar).

--- weedram/app.py
+++ weedram/app.py
@@ -1,6 +1,6 @@
 # A very simple Flask Hello World app for you to get started with...
 
-import env
+import os
 
 from flask import Flask
 
@@ -9,7 +9,7 @@ from go_http.metrics import MetricsApiClient
 app = Flask(__name__)
 
 def attach_metrics(app):
-    app.metrics_api = MetricsApiClient(env['WEEDRAM_VUMIGO_API_TOKEN'])
+    app.metrics_api = MetricsApiClient(os.environ['WEEDRAM_VUMIGO_API_TOKEN'])
 
 attach_metrics(app)
 
