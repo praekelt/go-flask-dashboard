d513001ba48567c9733eafec46351027dc78a6de
Simon Cross <hodgestar+github@gmail.com>
2015-07-30 14:56:18 +0200

    Attempt to retrieve metrics (@hodgestar @rudigiesler & nelsonkoma)

--- weedram/app.py
+++ weedram/app.py
@@ -1,9 +1,31 @@
 # A very simple Flask Hello World app for you to get started with...
 
+import env
+
 from flask import Flask
+
+from go_http.metrics import MetricsApiClient
 
 app = Flask(__name__)
 
+def attach_metrics(app):
+    app.metrics_api = MetricsApiClient(env['WEEDRAM_VUMIGO_API_TOKEN'])
+
+attach_metrics(app)
+
 @app.route('/')
 def hello_world():
-    return 'Weedram: A dash from a flask.'
+    metrics = app.metrics_api.get_metric(
+        '.*', start='-7d', interval='1d', nulls='omit')
+    metrics_html = str(metrics)
+    html = """<html>
+<head>
+    <title>Weedram: A dash from a flask.</title>
+</head>
+<body>
+    <h1>Metrics</h1>
+    %s
+</body>
+</html>
+    """ % metrics_html
+    return html
